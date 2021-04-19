2c9bfa04cb567802624e36ae30ed4feb91eeb546
geralt678 <81981167+geralt678@users.noreply.github.com>
2021-04-19 14:59:49 +0200

    add example for secure access to endpoints

--- spring/src/main/java/com/geralt/keycloak/controller/HomeController.java
+++ spring/src/main/java/com/geralt/keycloak/controller/HomeController.java
@@ -1,25 +1,21 @@
 package com.geralt.keycloak.controller;
 
-
-
 import javax.annotation.security.RolesAllowed;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@AllArgsConstructor
 public class HomeController {
 
-  @RolesAllowed("user")
+  @RolesAllowed({"user"})
   @GetMapping("/user-data")
   public Data userData() {
     return new Data("User data");
   }
 
-  @RolesAllowed("admin")
+  @RolesAllowed({"admin"})
   @GetMapping("/admin-data")
   public Data adminData() {
     return new Data("Admin data");
--- spring/src/main/java/com/geralt/keycloak/security/SecurityConfig.java
+++ spring/src/main/java/com/geralt/keycloak/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.geralt.keycloak.security;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
 import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
@@ -16,6 +17,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -31,7 +33,10 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) {
-    auth.authenticationProvider(keycloakAuthenticationProvider());
+    KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+    auth.authenticationProvider(keycloakAuthenticationProvider);
+
   }
 
   @ConditionalOnMissingBean
